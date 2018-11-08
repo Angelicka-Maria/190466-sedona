1e4ec04d112de034445318bd7ce232cf155289ad
Литвиненко Юрий <litvinenko-don@ukr.net>
2018-11-08 17:07:52 +0300

    Очердная поделка :-)

---
+++ source/js/script.js
@@ -0,0 +1,14 @@
+"use strict";
+var btnMenuClose = document.querySelector(".main-nav__btn-close");
+var menuMob = document.querySelector(".nav-list");
+
+/*события по кнопке "закрыть меню"*/
+btnMenuClose.addEventListener("click", function (event)
+  event.preventDefault();
+  console.log("касание на кнопку");
+
+  /*закрыть меню*/
+  menuMob.classList.add("nav-list--close");
+ /*закрыть кнопку*/
+  btnMenuClose.classList.add(".main-nav__btn-close--close");
+);
