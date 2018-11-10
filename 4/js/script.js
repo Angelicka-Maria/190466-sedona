cee698be421fed7971af340309d6dc991ec7a08d
Keks <keks@htmlacademy.ru>
2018-11-10 14:11:05 +0300

    :heavy_check_mark: Сборка #4

--- 4/js/script.js
+++ 4/js/script.js
@@ -3,7 +3,7 @@ var btnMenuClose = document.querySelector(".main-nav__btn-close");
 var menuMob = document.querySelector(".nav-list");
 
 /*события по кнопке "закрыть меню"*/
-btnMenuClose.addEventListener("click", function (event)
+btnMenuClose.addEventListener("click", function (event){
   event.preventDefault();
   console.log("касание на кнопку");
 
@@ -11,4 +11,4 @@ btnMenuClose.addEventListener("click", function (event)
   menuMob.classList.add("nav-list--close");
  /*закрыть кнопку*/
   btnMenuClose.classList.add(".main-nav__btn-close--close");
-);
+});
